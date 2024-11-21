c68aa243c1de97ddc51ac0d9c3a4f4f793d5228d
Mindaugas Puniskis <mindaugas.puniskis@ondato.com>
2024-11-21 12:40:26 +0200

    binaries

---
+++ Package.swift
@@ -0,0 +1,45 @@
+// swift-tools-version: 5.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "OndatoSDK",
+    platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        .library(name: "OndatoSDK",
+                 targets: [
+                    "OndatoSDK"
+                 ]),
+        .library(name: "FaceTecSDK",
+                 targets: [
+                    "FaceTecSDK"
+                 ]),
+        .library(name: "OndatoNFC",
+                 targets: [
+                    "OndatoNFC"
+                 ]),
+        .library(name: "OpenSSL",
+                 targets: [
+                    "OpenSSL"
+                 ]),
+        .library(name: "OndatoScreenRecorder",
+                 targets: [
+                    "OndatoScreenRecorder"
+                 ])
+    ],
+    targets: [
+        .binaryTarget(name: "OndatoSDK",
+                      path: "Binaries/OndatoSDK.xcframework"),
+        .binaryTarget(name: "FaceTecSDK",
+                      path: "Binaries/FaceTecSDK.xcframework"),
+        .binaryTarget(name: "OndatoNFC",
+                      path: "Binaries/OndatoNFC.xcframework"),
+        .binaryTarget(name: "OpenSSL",
+                      path: "Binaries/OpenSSL.xcframework"),
+        .binaryTarget(name: "OndatoScreenRecorder",
+                      path: "Binaries/OndatoScreenRecorder.xcframework")
+    ]
+)
